399db10303ee73a7a6256c6fdaf74b4e9ee317bd
matthew-sessions <matthewlsessions@gmail.com>
2022-02-12 17:31:10 -0800

    added files

--- CommerceApi/config/__init__.py
+++ CommerceApi/config/__init__.py
@@ -10,10 +10,3 @@ class Config:
     base_url = config("base_url")
     deta_key = config("deta_key", None)
     stripe_key = config("stripe_key")
-
-
-# base_page_id = str(uuid.UUID("6e90b64899424171af9975d4688d12d6"))
-# notion_token = "secret_sUSp4QpPDl36tfbZzFQwEyqCOSrzirqWB13aFKepgps"
-# base_url = "https://r917zp.deta.dev"
-# deta_key = "b0rs4bo3_g4mpnBvW35e4XUBb5dPS8DQokmfGp3uG"
-# stripe_key = "sk_test_x2b3B8YCbUOVizzq64f5wafN"
